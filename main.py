69bc1fb2e64ff34671b9888ebf1ee8d0f6a91321
Jean Michel <jean.michel@ebizz.com.br>
2021-01-12 11:18:25 -0300

    removed comments

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from hyphen import Hyphenator
 from hyphen.dictools import *
 
-# Download and install some dictionaries in the default directory using the default
-# repository, usually the OpenOffice website
 for lang in ['pt_BR']:
     if not is_installed(lang): install(lang)
-# Create some hyphenators
+
 pt_br = Hyphenator('pt_BR')
 
 word = input("Digite a palavra: ")
